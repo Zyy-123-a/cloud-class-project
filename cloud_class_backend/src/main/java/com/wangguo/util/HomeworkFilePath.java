ff54d112f42277a01aa309668b7df160c0d3b6c7
ylu <ylu070320@gmail.com>
2026-06-29 10:05:02 +0800

    Revert "作业首页"
    This reverts commit 965f8ab97f6e8e0eda8ed29170b918a85039cfb9.

--- cloud_class_backend/src/main/java/com/wangguo/util/HomeworkFilePath.java
+++
@@ -1,8 +0,0 @@
-package com.wangguo.util;
-
-/**
- * 模块2：作业附件存储路径（独立配置，不修改 DefaultInfo）
- */
-public class HomeworkFilePath {
-    public static final String HOMEWORK_DIR = "E:\\computer2\\file\\homework";
-}
